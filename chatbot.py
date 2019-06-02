a98610d7a2f656bd2c273180c676c659b8f9ae5f
Young Ahn <young_ahn@yonsei.ac.kr>
2019-06-02 16:07:29 +0900

    Mark XII
    Multiple Ticket Infromation

--- chatbot.py
+++ chatbot.py
@@ -2,25 +2,26 @@ import sys
 import chatbotmodel
 import telegram
 from pymongo import MongoClient
+import pprint
 
 #database setting
 client = MongoClient('localhost',27017)
 db = client.dbsparta
 melon_ticket = db.melon_ticket.find() #indexing from database
-dbaccess = db.melon_ticket.find_one()
+dbaccess_an_item = db.melon_ticket.find_one()
+dbaccess_multiple_item = db.melon_ticket.find({})
 
+#token settings
 baekcloud_token = '733519455:AAFn6_CUmVo2GCYz6Y9sl3JpuoZoJ_U2PWo'
 baekcloud = telegram.Bot(token = baekcloud_token)
 updates = baekcloud.getUpdates()
 
-def firecracker():
-    return ''
 
 def proc_ticket(bot, update):
-    message = str(dbaccess['title']) + "\n" + str(dbaccess['url'])
-    baekcloud.sendMessage(message)
-    sound = firecracker()
-    baekcloud.sendMessage(sound)
+    for object in db.melon_ticket.find():
+        message = str(object['title']) + "\n" + str(object['url'])
+        #print(message)
+        baekcloud.sendMessage(message)
 
 def proc_stop(bot, update):
     baekcloud.sendMessage('Bye Bye My Blue')
@@ -31,4 +32,3 @@ baekcloud= chatbotmodel.baekcloud_bot()
 baekcloud.add_handler('ticket', proc_ticket)
 baekcloud.add_handler('stop', proc_stop)
 baekcloud.start()
-
