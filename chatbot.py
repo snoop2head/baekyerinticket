3f245ae5d85cf9eca4050abd267a377c95554620
Young Ahn <young_ahn@yonsei.ac.kr>
2019-06-02 14:50:26 +0900

    Mark X

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,34 @@
+import sys
+import chatbotmodel
 import telegram
+from pymongo import MongoClient
+
+#database setting
+client = MongoClient('localhost',27017)
+db = client.dbsparta
+melon_ticket = db.melon_ticket.find() #indexing from database
 
 baekcloud_token = '733519455:AAFn6_CUmVo2GCYz6Y9sl3JpuoZoJ_U2PWo'
 baekcloud = telegram.Bot(token = baekcloud_token)
 updates = baekcloud.getUpdates()
-for u in updates:
-    print(u.message)
+
+def firecracker():
+    return 'ㅇㅅㅇ'
+
+def proc_rolling(bot, update):
+    msg = db.melon_ticket.find_one()
+    baekcloud.sendMessage(msg['title'])
+    sound = firecracker()
+    baekcloud.sendMessage(sound)
+    baekcloud.sendMessage('르르..')
+
+def proc_stop(bot, update):
+    baekcloud.sendMessage('Bye Bye My Blue')
+    baekcloud.stop()
+
+
+baekcloud= chatbotmodel.baekcloud_bot()
+baekcloud.add_handler('rolling', proc_rolling)
+baekcloud.add_handler('stop', proc_stop)
+baekcloud.start()
+
---
+++ chatbotmodel.py
@@ -0,0 +1,32 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+class TelegramBot:
+    def __init__(self, name, token):
+        self.core = telegram.Bot(token)
+        self.updater = Updater(token)
+        self.id = 489724409
+        self.name = name
+
+    def sendMessage(self, text):
+        self.core.sendMessage(chat_id = self.id, text=text)
+
+    def stop(self):
+        self.updater.start_polling()
+        self.updater.dispatcher.stop()
+        self.updater.job_queue.stop()
+        self.updater.stop()
+
+class baekcloud_bot(TelegramBot):
+    def __init__(self):
+        self.token = '733519455:AAFn6_CUmVo2GCYz6Y9sl3JpuoZoJ_U2PWo'
+        TelegramBot.__init__(self, 'baekcloud', self.token)
+        self.updater.stop()
+
+    def add_handler(self, cmd, func):
+        self.updater.dispatcher.add_handler(CommandHandler(cmd, func))
+
+    def start(self):
+        self.sendMessage('Hi Hi My Red')
+        self.updater.start_polling()
+        self.updater.idle()
